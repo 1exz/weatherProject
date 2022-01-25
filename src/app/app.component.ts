7206f79326cc66fdf883781fa18105465634b17e
1exz <alex.buchko.m@gmail.com>
2022-01-25 17:07:35 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app';
+  cities:any;
+  weather:any;
+  showWeatherInCity:boolean=false;
+
+  constructor(private http: HttpClient) { }
+ 
+  showCities() {
+    const domain = 'http://api.airvisual.com';
+    const endPointAllCities = '/v2/cities?state=California&country=USA';
+    const APIKey = 'e4ebc3d2-4bb6-429a-aeca-4bee88f58e17';
+    const url = `${domain}${endPointAllCities}&key=${APIKey}`;
+
+    this.http.get(url).subscribe((response: any) => {
+      this.cities = response.data.map((response: any) => response.city);
+       console.log(this.cities);
+    })
+  }
+    showWeather(city: any) {
+      this.showWeatherInCity = true ;
+     
+      const domain = 'http://api.airvisual.com';
+      const endPointCity = '/v2/city';
+      const APIKey = 'e4ebc3d2-4bb6-429a-aeca-4bee88f58e17';
+      const urlCity = `${domain}${endPointCity}`;
+      const APIParams = {
+        city: city,
+        state: 'California',
+        country: 'USA',
+        key: APIKey,
+      };
+  
+      this.http
+        .get(urlCity, { params: APIParams })
+        .pipe(map((response: any) => response.data.current.weather))
+        .subscribe((data: any) => {
+          this.weather = data.tp;
+          console.log(this.weather)
+        });
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { Component, NgModule } from '@angular/core';
+import { Routes,RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NewsComponent } from './news/news.component';
+import { PriceComponent } from './price/price.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NewServiceService } from './new-service.service';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from'@angular/common/http';
+
+  const appRoutes:Routes=[
+    {path:'', component:HomeComponent},
+    {path:'about', component:AboutComponent},
+    {path:'news', component:NewsComponent},
+    {path:'price', component:PriceComponent},
+    {path:'**', component:NotFoundComponent}
+  ]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NewsComponent,
+    PriceComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [NewServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
